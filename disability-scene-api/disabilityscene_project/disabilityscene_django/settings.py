76b2748d13b9e7f0e75e616b3c7ce3a4af42c5da
Margaret Camilletti <mhcamilletti@gmail.com>
2023-07-22 19:28:17 -0400

    updated requirements

--- disability-scene-api/disabilityscene_project/disabilityscene_django/settings.py
+++ disability-scene-api/disabilityscene_project/disabilityscene_django/settings.py
@@ -39,14 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'disabilityscene',
-    'rest_framework',
-    'corsheaders'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -141,18 +139,5 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:5173",
-    "http://127.0.0.1:5555",
-]
-
-CORS_ALLOW_ALL_ORIGINS = True
-
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
-    'http://disabilityscene.com'
-]
 
 ALLOWED_HOSTS = ['*']
