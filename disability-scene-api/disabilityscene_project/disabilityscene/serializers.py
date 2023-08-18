83f17220090d2ea6943ffca2b131de221ce4bf13
Margaret Camilletti <mhcamilletti@gmail.com>
2023-08-18 16:46:56 -0400

    updated serializers

--- disability-scene-api/disabilityscene_project/disabilityscene/serializers.py
+++ disability-scene-api/disabilityscene_project/disabilityscene/serializers.py
@@ -36,6 +36,5 @@ class ReviewSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'movie', 'title', 'reviewer_name', 'review_text', 'date', 'casting_score', 'character_score', 'originality_score', 'accuracy_score')
 
     def create(self, validated_data):
-        validated_data.pop('id', None)
         print(validated_data)
         return Review.objects.create(**validated_data)
